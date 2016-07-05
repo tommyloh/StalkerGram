f7a161ceee5bd416e1291cc5f002317b41f2b7c7
Tommy Loh <Tommy@tommys-macbook-air-879.local>
2016-07-05 14:10:39 +0800

    start new projects

--- instagram/LoginViewController.swift
+++ instagram/LoginViewController.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
     @IBAction func backToLogin(segue: UIStoryboardSegue){
         
     }
