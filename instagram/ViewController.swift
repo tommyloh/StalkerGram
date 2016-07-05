2610a6eeb42b7350337e4b55965ccff734405a9c
Tommy Loh <Tommy@tommys-macbook-air-879.local>
2016-07-05 10:35:44 +0800

    Initial Commit

---
+++ instagram/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by Tommy Loh on 04/07/2016.
+//  Copyright © 2016 Tommy Loh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func backToLogin(segue: UIStoryboardSegue){
+        
+    }
+
+    }
+
+
